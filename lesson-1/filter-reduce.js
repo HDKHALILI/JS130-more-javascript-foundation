9613e6d90b086b7446363d358a6d12b78c4d52b4
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-10-28 09:11:41 +1100

    add filter-reduce.js

---
+++ lesson-1/filter-reduce.js
@@ -0,0 +1,18 @@
+function filter(arr, callback) {
+  return arr.reduce((results, element) => {
+    if (callback(element)) {
+      results.push(element);
+    }
+
+    return results;
+  }, []);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(filter(numbers, number => number > 3)); // => [ 4, 5 ]
+console.log(filter(numbers, number => number < 0)); // => []
+console.log(filter(numbers, () => true)); // => [ 1, 2, 3, 4, 5 ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(filter(values, value => typeof value === "string"));
+// => [ 'abc', 'xyz' ]
